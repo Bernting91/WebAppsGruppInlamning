97efa93d2ae16f6db022857cc5c4d8d1fd296cf2
Emil Lindhult <emillindhult@gmail.com>
2024-04-25 15:24:01 +0200

    added car id, testing push

--- WebAppsGruppInlamning/obj/Debug/net8.0/WebAppsGruppInlamning.AssemblyInfo.cs
+++ WebAppsGruppInlamning/obj/Debug/net8.0/WebAppsGruppInlamning.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppsGruppInlamning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ece9f27e5c3b27a4f2c97ba5ef19ac6961e6de")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppsGruppInlamning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppsGruppInlamning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
