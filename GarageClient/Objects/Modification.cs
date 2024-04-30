cd3c352f0bd8911192bf8b709d528dec052b725a
Emil Lindhult <emillindhult@gmail.com>
2024-04-30 10:40:04 +0200

    Added Client Project with Car and Modification Objects

---
+++ GarageClient/Objects/Modification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageClient.Objects
+{
+    public class Modification
+    {
+        public int ModificationId { get; set; }
+        public string ModificationName { get; set; }
+
+        public Modification(int modificationId, string modificationName)
+        {
+            ModificationId = modificationId;
+            ModificationName = modificationName;
+        }
+
+        public Modification() { }
+    }
+}
---
+++ GarageClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
