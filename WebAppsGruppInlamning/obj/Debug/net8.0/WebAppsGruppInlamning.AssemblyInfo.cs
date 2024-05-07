d08df28eebdb4a64cddba291a6d2ff75c10cd0e3
hahnhoffen <tim.hahnc@gmail.com>
2024-05-07 18:28:29 +0200

    last pull

--- WebAppsGruppInlamning/obj/Debug/net8.0/WebAppsGruppInlamning.AssemblyInfo.cs
+++ WebAppsGruppInlamning/obj/Debug/net8.0/WebAppsGruppInlamning.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppsGruppInlamning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03ce369018ecd0105516cd0df5c6b81f72c51806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24319683118e657fbf0874181dde5c787a27054d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppsGruppInlamning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppsGruppInlamning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
